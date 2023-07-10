ddb9ad4878b1230e402f769601af957f619ddd2b
nowaythatworked <jan.czekala99@web.de>
2023-07-10 15:43:29 +0200

    feat: conditional adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'
-import node from '@astrojs/node'
-import react from '@astrojs/react'
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({ mode: 'standalone' }),
-    integrations: [react()]
+  output: "server",
+  adapter: process.env.VERCEL ? vercel() : node({ mode: "standalone" }),
+  integrations: [react()],
 });
